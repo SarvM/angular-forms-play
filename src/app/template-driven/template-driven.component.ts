3e1c137c3905869adcb12ac9e7e38f54f0e53ae4
SarvM <saravana.msi@outlook.com>
2019-11-07 01:31:27 +0530

    final file

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Contact {
+  constructor(
+    fname: string,
+    email: string,
+    comment: string
+  ) {}
+}
+
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
@@ -7,9 +15,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenComponent implements OnInit {
 
+  model = new Contact('','','');
+
   constructor() { }
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
 
   ngOnInit() {
   }
+
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
